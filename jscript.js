f571ef02d76c339db04ae9269b8a295832c6b637
Aya Osama <ayaramadan2011@gmail.com>
2024-12-21 00:52:23 +0200

    design layout

---
+++ jscript.js
@@ -0,0 +1,46 @@
+const ques = [{
+    question: "What is the main ingredient in a classic hamburger?",
+    answer: [
+      { text: "Beef", correct: true },
+      { text: "Chicken", correct: false },
+      { text: "Pork", correct: false },
+      { text: "Turkey", correct: false },
+    ],},
+  {
+    question: "Which of these is a common topping on a hamburger?",
+    answer: [
+      { text: "Lettuce", correct: true },
+      { text: "Tomato", correct: true },
+      { text: "Onion", correct: true },
+      { text: "All of the above", correct: true },
+    ],
+  },
+  {
+    question: "What type of bun is typically used for a hamburger?",
+    answer: [
+      { text: "Brioche", correct: false },
+      { text: "Sesame Seed", correct: true },
+      { text: "Whole Wheat", correct: false },
+      { text: "Potato", correct: false },
+    ],
+  },
+  {
+    question: "Which of these is a popular cheese choice for a cheeseburger?",
+    answer: [
+      { text: "American", correct: true },
+      { text: "Cheddar", correct: true },
+      { text: "Swiss", correct: true },
+      { text: "All of the above", correct: true },
+    ],
+
+  },
+  {
+    question: "What is a common condiment for a hamburger?",
+    answer: [
+      { text: "Ketchup", correct: true },
+      { text: "Mustard", correct: true },
+      { text: "Mayonnaise", correct: true },
+      { text: "All of the above", correct: true },
+    ],
+  }
+];
