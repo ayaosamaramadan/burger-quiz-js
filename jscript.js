4e85cbbb2d52c0e2c696c203ed1d9d2f221a59e7
Aya Osama <ayaramadan2011@gmail.com>
2024-12-21 22:10:39 +0200

    show questions&answer

--- jscript.js
+++ jscript.js
@@ -1,11 +1,13 @@
-const ques = [{
+const ques = [
+  {
     question: "What is the main ingredient in a classic hamburger?",
     answer: [
       { text: "Beef", correct: true },
       { text: "Chicken", correct: false },
       { text: "Pork", correct: false },
       { text: "Turkey", correct: false },
-    ],},
+    ],
+  },
   {
     question: "Which of these is a common topping on a hamburger?",
     answer: [
@@ -32,7 +34,6 @@ const ques = [{
       { text: "Swiss", correct: true },
       { text: "All of the above", correct: true },
     ],
-
   },
   {
     question: "What is a common condiment for a hamburger?",
@@ -42,5 +43,47 @@ const ques = [{
       { text: "Mayonnaise", correct: true },
       { text: "All of the above", correct: true },
     ],
-  }
+  },
 ];
+
+let score = 0;
+let currQues = 0;
+let startBtn = document.getElementById("start");
+let questio = document.getElementById("ques");
+
+function start() {
+  // hide the start button
+  startBtn.style.display = "none";
+  // show the questions
+  questio.style.display = "block";
+  // show title of the question
+  let title = document.createElement("h1");
+  title.innerHTML = ques[currQues].question;
+  title.classList.add("text-2xl", "font-bold", "mb-4", "text-neutral-700");
+  questio.appendChild(title);
+  // show the answers
+  let btnsDiv = document.createElement("div");
+  btnsDiv.setAttribute("id", "btns");
+  btnsDiv.classList.add("grid", "gap-3");
+  ques[currQues].answer.forEach((ans) => {
+    let btn = document.createElement("button");
+    btn.innerHTML = ans.text;
+    btn.classList.add(
+      "bg-blue-500",
+      "hover:bg-blue-600",
+      "text-white",
+      "font-bold",
+      "py-2",
+      "px-4",
+      "rounded",
+      "transition",
+      "duration-300",
+      "ease-in-out",
+      "transform",
+      "hover:scale-105"
+    );
+    btnsDiv.appendChild(btn);
+  });
+
+  questio.appendChild(btnsDiv);
+}
